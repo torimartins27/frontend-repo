8a1d461fa8b8dd43983f3a6b31ed3a846159e879
VICTORIA MARTINS <TORIMARTINS27@GMAIL.COM>
2025-05-13 15:20:33 -0400

    Gallery for main page added, retrieving artwork from API

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,51 @@
+import { useEffect, useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import "./App.css";
 import Header from "../Header/Header";
+import ArtworkDetail from "../ArtworkDetail/ArtworkDetail";
+import Gallery from "../Gallery/Gallery";
+import "./App.css";
+
+import { fetchFeaturedArtworks } from "../../utils/api";
+
 import Main from "../Main/Main";
 
 function App() {
+  const [artworks, setArtworks] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const loadArtworks = async () => {
+      const data = await fetchFeaturedArtworks();
+      setArtworks(data);
+    };
+
+    loadArtworks();
+  }, []);
+
+  const handleArtworkClick = (id) => {
+    navigate(`/artwork/${id}`);
+  };
+
   return (
     <div className="page">
       <div className="page_content">
         <Header />
-        <Main />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <Gallery
+                artworks={artworks}
+                onArtworkClick={handleArtworkClick}
+              />
+            }
+          />
+          <Route
+            path="/artwork/:id"
+            element={<ArtworkDetail artworks={artworks} />}
+          />
+        </Routes>
       </div>
     </div>
   );
--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,12 +1,33 @@
+import React from "react";
 import "./Gallery.css";
 
-function Gallery() {
+function Gallery({ artworks, onArtworkClick }) {
   return (
-    <section className="gallery">
-      <div className="gallery__container-left">
-        <h2>Featured Artworks</h2>
+    <section className="gallery-section">
+      <h2 className="gallery-title">Featured Artworks</h2>
+      <div className="gallery">
+        {artworks.length > 0 ? (
+          artworks.map((artwork) => (
+            <div
+              key={artwork.id}
+              className="gallery-item"
+              onClick={() => onArtworkClick(artwork.id)}
+            >
+              <img
+                src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/843,/0/default.jpg`}
+                alt={artwork.title}
+                className="artwork-image"
+              />
+              <div className="artwork-info">
+                <h3>{artwork.title}</h3>
+                <p>{artwork.artist_display}</p>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>Loading artworks...</p> // Message in case artwork data hasn't loaded yet
+        )}
       </div>
-      <div className="gallery__items"></div>
     </section>
   );
 }
