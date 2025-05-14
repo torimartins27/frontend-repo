1aad5a298310a3a35e98461160ca6ac4d72da667
VICTORIA MARTINS <TORIMARTINS27@GMAIL.COM>
2025-05-13 20:11:26 -0400

    Added menu modal component and artwork details componen

--- src/components/ArtworkDetail/ArtworkDetail.jsx
+++ src/components/ArtworkDetail/ArtworkDetail.jsx
@@ -1,7 +1,61 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import striptags from "striptags";
 import "./ArtworkDetail.css";
 
 function ArtworkDetail() {
-  return;
+  const { id } = useParams();
+  const [artwork, setArtwork] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchArtworkDetail = async () => {
+      try {
+        const response = await fetch(
+          `https://api.artic.edu/api/v1/artworks/${id}`
+        );
+        const data = await response.json();
+        setArtwork(data.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching artwork details:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchArtworkDetail();
+  }, [id]);
+
+  if (loading) return <div>Loading...</div>;
+  if (!artwork) return <div>Artwork not found.</div>;
+
+  return (
+    <div className="artwork-detail">
+      <h2>{artwork.title}</h2>
+      <p>{artwork.artist_display}</p>
+      {artwork.image_id ? (
+        <img
+          src={`https://www.artic.edu/iiif/2/${artwork.image_id}/full/1200,/0/default.jpg`}
+          alt={artwork.title}
+          className="selectedArtwork-image"
+        />
+      ) : (
+        <div className="image-placeholder">Image Not Available</div>
+      )}
+      <p>
+        <strong>Creation Date:</strong> {artwork.date_display || "Unknown"}
+      </p>
+      <p>
+        <strong>Medium:</strong> {artwork.medium_display || "Not Available"}
+      </p>
+      <p>
+        <strong>Description:</strong>{" "}
+        {artwork.description
+          ? striptags(artwork.description)
+          : "No description available."}
+      </p>
+    </div>
+  );
 }
 
 export default ArtworkDetail;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,40 @@
 import "./Header.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
 import logo from "../../assets/images/logo.png";
+import MenuModal from "../MenuModal/MenuModal";
+import burgerMenu from "../../assets/images/burgerMenu.svg";
 
 function Header() {
+  const location = useLocation();
+
+  const isNoBgPage =
+    location.pathname.startsWith("/artwork") ||
+    location.pathname.startsWith("/paintings") ||
+    location.pathname.startsWith("/form") ||
+    location.pathname.startsWith("/textiles");
+
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
-    <header className="header header--fade-in">
-      <div className="header__container-left">
+    <header className={`header header--fade-in ${isNoBgPage ? "no-bg" : ""}`}>
+      <div className="header__container">
         <Link to="/">
           <img
-            className="header__logo"
+            className={`header__logo ${isNoBgPage ? "small-logo" : ""}`}
             src={logo}
             alt=" Chicago Art Institute Logo"
           />
         </Link>
+        <img
+          src={burgerMenu}
+          alt="Menu"
+          className="menu-icon"
+          onClick={() => setMenuOpen(true)}
+        />
       </div>
+      {/* <h3 className="header__welcome">WELCOME</h3> */}
+      {menuOpen && <MenuModal onClose={() => setMenuOpen(false)} />}
     </header>
   );
 }
